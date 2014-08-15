2822d87cc4af06449fb93e83209735ebe372befe
Matthew <matthew.j.dailey@gmail.com>
2014-08-14 21:06:41 -0700

    Added basic app flow

--- eclipse/src/com/softmaybe/MainActivity.java
+++ eclipse/src/com/softmaybe/MainActivity.java
@@ -1,22 +1,23 @@
 package com.softmaybe;
 
+import com.softmaybe.util.Prefs;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
 	private final static String TAG = "MainActivity";
-	
-	private final static String PREFS_FILENAME = "softmaybe.prefs";
-	private final static String EMAIL_PREF = "email";
-	
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
 	    // Get intent, action and MIME type
 	    Intent intent = getIntent();
@@ -24,7 +25,7 @@ public class MainActivity extends ActionBarActivity {
 	    		"text/plain".equals(intent.getType())) {
             handleSendText(intent); 
 	    } else {
-	    	showEmailInput();
+	    	fillInEmailInput();
 	    }
 	}
 
@@ -32,15 +33,46 @@ public class MainActivity extends ActionBarActivity {
 	    String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
 	    if (sharedText != null) {
 	    	Log.i(TAG, "Received share: " + sharedText);
+	    	showSharedText(sharedText);
+			String email = fillInEmailInput();	    	
+	    	if (!email.equals("")) {
+	    		storeReminderAndClose(email, sharedText);
+	    	}
 	    }
 	}
+
+	private void showSharedText(String shared) {
+		TextView eventText = (TextView) findViewById(R.id.event_text);
+		eventText.setText(shared);
+	}
 	
-	private void showEmailInput() {
-		setContentView(R.layout.activity_main);
+	private String fillInEmailInput() {
+		TextView emailInput = (TextView) findViewById(R.id.email_input);
+		String email = Prefs.getEmail(getBaseContext());
+		emailInput.setText(email);
+		return email;
 	}
 	
 	private void saveEmailAndClose() {
-		
+		TextView emailInputView = (TextView) findViewById(R.id.email_input);
+		String email = emailInputView.getText().toString();
+		Prefs.setEmail(email, getBaseContext());
+
+		// If the event text is set, we'll try to store an event.
+		TextView eventTextView = (TextView) findViewById(R.id.event_text);
+		String eventText = eventTextView.getText().toString();
+		if (!eventText.equals(getString(R.string.default_event_text))) {
+			storeReminderAndClose(email, eventText);
+		} else {
+			finish();
+		}
+	}
+	
+	private void storeReminderAndClose(String email, String sharedText) {
+		// TODO (matt): Store a reminder in softmaybe.com.
+		Log.i(TAG, "Setting reminder for sharedText:'" + sharedText + "' and"
+				+ " email:" + email);
+		finish();
 	}
 
 	@Override
@@ -57,6 +89,7 @@ public class MainActivity extends ActionBarActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_save) {
+			saveEmailAndClose();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
